1999e256f82a5f488a3b742b7a6920ba9b1176fa
Syber88 <siyamgz88@gmail.com>
2025-03-05 13:57:29 +0200

    added redeem and deposit methods. Also created interface

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,19 +1,39 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.24;
 
+import {IRebaseToken} from "./interfaces/IRebaseToken.sol";
+
 contract Vault {
+    error Vault__RedeemFailed();
 
-    address private immutable i_rebaseToken;
+    IRebaseToken private immutable i_rebaseToken;
 
-    constructor(address _rebaseToken) {
+    event Deposit(address indexed sender, uint256 amount);
+    event Redeem(address indexed sender, uint256 amount);
+
+    constructor(IRebaseToken _rebaseToken) {
         i_rebaseToken = _rebaseToken;
     }
 
     function getRebaseTokenAddress() external view returns(address) {
-        return i_rebaseToken;
+        return address(i_rebaseToken);
     }
 
     receive() external payable {
 
+    }
+
+    function deposit () external payable {
+        i_rebaseToken.mint(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function redeem(uint256 _amount) external {
+        i_rebaseToken.burn(msg.sender, _amount);
+        (bool success,) = payable(msg.sender).call{value: _amount}("");
+        if (!success) {
+            revert Vault__RedeemFailed();
+        }
+        emit Redeem(msg.sender, _amount);
     }
 }
