58534af7ac3621fff28a8a2c9690fd00ccec4a28
Syber88 <siyamgz88@gmail.com>
2025-03-08 21:51:38 +0200

    eod push, errors not yet resolved

--- src/RebaseTokenPool.sol
+++ src/RebaseTokenPool.sol
@@ -5,7 +5,7 @@ import {TokenPool} from "@ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract RebaseTokenPool is TokenPool{
-    constructor(IERC20 _token, address[] memory _allowList, address _rmnProxy, address _router) TokenPool(_token, _allowList, _rmnProxy, _router) {
+    constructor(IERC20 _token, address[] memory _allowList, address _rmnProxy, address _router) TokenPool(_token, 18, _allowList, _rmnProxy, _router) {
 
     }
 
