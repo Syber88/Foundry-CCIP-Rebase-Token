1d15e703f4571b57297c7720a5b55c5d94cd9485
Syber88 <siyamgz88@gmail.com>
2025-03-09 22:36:08 +0200

    added token pool for sepolia

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -6,19 +6,48 @@ import {RebaseToken} from "../src/RebaseToken.sol";
 import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
 import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
 import {Vault} from "../src/Vault.sol";
-import {CCIPLocalSimulatorFork} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+import {CCIPLocalSimulatorFork, Register} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+import {IERC20} from "@ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+
 
 contract CrossChainTest is Test {
+    address owner = makeAddr("owner");
     uint256 sepoliaFork;
     uint256 arbSepoliaFork;
 
     CCIPLocalSimulatorFork ccipLocalSimFork;
 
+    RebaseToken sepoliaToken;
+    RebaseToken arbSepoliaToken;
+
+    Vault vault;
+    RebaseTokenPool sepoliaPool;
+    RebaseTokenPool ArbSepoliaPool;
+
+    Register.NetworkDetails sepoliaNetworkDetails;
+    Register.NetworkDetails arbSepoliaNetworkDetails;
+
     function setUp() public {
         sepoliaFork = vm.createSelectFork("sepolia");
-        arbSepoliaFork = vm.createSelectFork("arb-sepolia");
+        arbSepoliaFork = vm.createFork("arb-sepolia");
 
         ccipLocalSimFork = new CCIPLocalSimulatorFork();
         vm.makePersistent(address(ccipLocalSimFork));
+        //deploy and configure on sepolia
+        sepoliaNetworkDetails = ccipLocalSimFork.getNetworkDetails(block.chainid);
+        vm.startPrank(owner);
+        sepoliaToken = new RebaseToken();
+        vault = new Vault(IRebaseToken(address(sepoliaToken)));
+        sepoliaPool = new RebaseTokenPool(IERC20(address(sepoliaToken)),new address[](0), sepoliaNetworkDetails.rmnProxyAddress, sepoliaNetworkDetails.routerAddress);
+        vm.stopPrank();
+
+        //deploy and configure on arb sepolia
+        vm.selectFork(arbSepoliaFork);
+        sepoliaNetworkDetails = ccipLocalSimFork.getNetworkDetails(block.chainid);
+        arbSepoliaToken = new RebaseToken();
+
+        vm.startPrank(owner);
+        vm.stopPrank();
+
     }
 }
