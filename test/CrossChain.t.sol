0c227b7c89fb5a0248c46942580d9d4ce39deef3
Syber88 <siyamgz88@gmail.com>
2025-03-09 19:08:11 +0200

    finished integrating ccip local sim for forked chains

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -6,14 +6,22 @@ import {RebaseToken} from "../src/RebaseToken.sol";
 import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
 import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
 import {Vault} from "../src/Vault.sol";
+import {CCIPLocalSimulatorFork} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
 
 contract CrossChainTest is Test{
     uint256 sepoliaFork;
     uint256 arbSepoliaFork;
 
+    CCIPLocalSimulatorFork ccipLocalSimFork;
+
+
     function setUp() public {
         sepoliaFork = vm.createSelectFork("sepolia");
         arbSepoliaFork = vm.createSelectFork("arb-sepolia");
+
+        ccipLocalSimFork = new CCIPLocalSimulatorFork();
+        vm.makePersistent(address(ccipLocalSimFork));
+
         
     }
 
