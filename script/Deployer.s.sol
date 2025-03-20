0f7c25fa74a70e63c792ba9c013b1648843e2800
Syber88 <siyamgz88@gmail.com>
2025-03-20 09:00:00 +0200

    initialised deployment of tokens and token pools

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -4,8 +4,21 @@ pragma solidity ^0.8.24;
 import {Script} from "forge-std/Script.sol";
 import {Vault} from "../src/Vault.sol";
 import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
+import {RebaseToken} from "../src/RebaseToken.sol";
+import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
+import {CCIPLocalSimulatorFork, Register} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+import {IERC20} from "@ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+
 
 contract TokenAndPoolDeployer is Script {
+    function run() public returns (RebaseToken token, RebaseTokenPool pool) {
+        CCIPLocalSimulatorFork ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
+        Register.NetworkDetails memory networkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainid);
+        vm.startBroadcast();
+        token = new RebaseToken();
+        pool = new RebaseTokenPool(IERC20(address(token)), new address[](0), networkDetails.rmnProxyAddress, networkDetails.routerAddress);
+        vm.stopBroadcast();
+    }
 
 }
 
